ef0dea26f88a017d955777dd6225d66ef0f11515
AkashMAbrrar <madborakash48@gmail.com>
2024-10-06 00:25:47 +0600

    this commit for complete the project and search videos with kewords

---
+++ js/summary.js
@@ -0,0 +1,13 @@
+/**
+ * 1: Load and display initially
+ * 2: on Click load and display data
+ * 3: Complex object accessing
+ * 4: toggle active class functionality
+ * 5: search functionality
+ * 6: load and display single data
+ * 7: display data in a modal
+ * 8: conditional rendering if array doesn't have any data
+ * 9: load and display single category data
+ * 10:conditional rendering if a value empty
+ * 11: conditional rendering if a value length is zero
+ * **/
